d0e5f596deab35e3dd1d92474d0befba4d0bedc5
jorgitorr <jorgearceruiz97@gmail.com>
2022-12-11 13:13:23 +0100

    ejercicio de monte de tres cartas arreglado

--- monte_de_tres_cartas_nuevo_intento.py
+++ monte_de_tres_cartas_nuevo_intento.py
@@ -6,13 +6,14 @@ CORAZONES = chr(9829)
 DIAMANTES = chr(9830)
 PICAS = chr(9824)
 TREBOL = chr(9827)
+
+NUMERO_CAMBIOS = 5 # numero de cambios que realiza el cupier (modificable)
+DELAY = 0.2 # velocidad con la que cambia las cartas(modificable)
 ''''''
 
-def get_carta_al_azar():
-    '''tengo que comprobar que las cartas son diferentes'''
-    palo = [CORAZONES,DIAMANTES,PICAS,TREBOL] 
-    numero = "123456789JK" 
-    return random.choice(str(numero)),random.choice(palo)
+def get_carta_azar():
+    '''tengo que comprobar que las cartas son diferentes''' 
+    return random.choice(list("123456789JK")+ ['10']),random.choice([CORAZONES,DIAMANTES,PICAS,TREBOL])
 
 
 def imprime(baraja):
@@ -41,12 +42,10 @@ def es_posicion(pos):
 if __name__=="__main__":
     '''variables'''
     jugadas = ["L-R","L-M","M-L","M-R","R-L","R-M"]# todas las posibles jugadas
-    baraja = [("Q",CORAZONES),(get_carta_al_azar()),(get_carta_al_azar())]
-    NUMERO_CAMBIOS = 5 # numero de cambios que realiza el cupier (modificable)
-    DELAY = 0.2 # velocidad con la que cambia las cartas(modificable)
+    baraja = [("Q",CORAZONES),(get_carta_azar()),(get_carta_azar())]
     '''fin de variables'''
 
-    get_carta_al_azar()
+    get_carta_azar()
     shuffle(baraja) # desordena las barajas antes de imprimir por primera vez
 
     print("Three-Card Monte\n\nFind the red lady (the Queen of Hearts)! Keep an eye on how\n"\
